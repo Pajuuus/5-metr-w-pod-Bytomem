d7b9fe30184e0c7037497433cd2ed887ece3b6b9
llumii <llumii@tutamail.com>
2026-09-08 13:06:57 +0200

    new official icon

--- grobowiec/obj/Debug/net10.0-android/android-arm64/grobowiec.AssemblyInfo.cs
+++ grobowiec/obj/Debug/net10.0-android/android-arm64/grobowiec.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("grobowiec")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b3113201a9005ccf194954148867119c3870ed45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ae50f227757bf19b7b52b2dc24b7bc0eb86caeb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("grobowiec")]
 [assembly: System.Reflection.AssemblyTitleAttribute("grobowiec")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
